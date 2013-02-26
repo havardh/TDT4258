a037665449d2de06be78552424a99cab06e87dac
Håvard Wormdal Høiby <havardwhoiby@gmail.com>
2013-02-26 18:11:49 +0100

    Button statevariable pressed

--- ov02/interrupt.c
+++ ov02/interrupt.c
@@ -1,5 +1,7 @@
 #include "interrupt.h"
 
+static int pressed = 0;
+
 void set_value(int v) {
 	// Turn off lights
 	pioc->codr = 0xFF;
@@ -8,48 +10,53 @@ void set_value(int v) {
 	pioc->sodr = v;
 }
 
+
 void rotate_left(void) {
 	LED_VALUE = (LED_VALUE << 1);
+
 }
 
 void rotate_right(void) {
 	LED_VALUE = (LED_VALUE >> 1);
+
+
 }
 
 __int_handler *button_isr(void) {
 	int led_status = piob->isr;
 
+	if (!pressed) {
 
+		switch(led_status) {
+		case SW0:
+			rotate_right();
+			break;
 
-	switch(led_status) {
-	case SW0:
-		rotate_right();
-		break;
+		case SW1:
+			set_value(0x1);
+			break;
 
-	case SW1:
-		set_value(0x1);
-		break;
+		case SW2:
+			rotate_left();
+			break;
 
-	case SW2:
-		rotate_left();
-		break;
+		case SW3:
+			set_value(0x2);
+			break;
 
-	case SW3:
-		set_value(0x2);
-		break;
+		case SW4:
+			break;
 
-	case SW4:
-		break;
+		case SW5:
+			break;
 
-	case SW5:
-		break;
+		case SW6:
+			break;
 
-	case SW6:
-		break;
-
-	case SW7:
-		break;
+		case SW7:
+			break;
+		}
 	}
-
+	pressed = ~pressed;
 	return 0;
 }
