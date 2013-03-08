350fbe3921e74a9ef2909318065855be04ced26f
Håvard Wormdal Høiby <havardwhoiby@gmail.com>
2013-03-08 11:53:06 +0100

    Removed init from decl

--- ov02/include/gpio.h
+++ ov02/include/gpio.h
@@ -8,10 +8,10 @@
 
 static uint8_t led_value;
 
-extern volatile avr32_pio_t *piob = &AVR32_PIOB;
-extern volatile avr32_pio_t *pioc = &AVR32_PIOC;
-extern volatile avr32_abdac_t *dac = &AVR32_ABDAC;
-extern volatile avr32_pm_t *sm = &AVR32_PM;
+extern volatile avr32_pio_t *piob;
+extern volatile avr32_pio_t *pioc;
+extern volatile avr32_abdac_t *dac;
+extern volatile avr32_pm_t *sm;
 
 // Init HW
 void init_intc( void );
