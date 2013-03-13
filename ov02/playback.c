a98a41bb24868b57c00a7f1d2591c3654e4f13d8
Håvard Wormdal Høiby <havardwhoiby@gmail.com>
2013-03-13 10:11:42 +0100

    Renamed variable

--- ov02/playback.c
+++ ov02/playback.c
@@ -2,26 +2,25 @@
 #include "playback.h"
 
 static int16_t (*sample_fn)(int) = square_sample;
-static int samples[TRACKS] = {0, 0, 0, 0};
-struct note_t** notes;
+static struct note_t** tracks;
 
 void init_tracks() {
-	notes = (note_t**) calloc (sizeof(note_t**), TRACKS);
-	memset(notes, NULL, TRACKS);
+	tracks = (note_t**) calloc (sizeof(note_t**), TRACKS);
+	memset(tracks, NULL, TRACKS);
 }
 
 void tracks_finalize() {
 
-	free (notes);
+	free (tracks);
 
 }
 
 void set_track(int track, note_t* n) {
-	notes[track] = n;
+	tracks[track] = n;
 }
 
 note_t* get_track(int track) {
-	return notes[track];
+	return tracks[track];
 }
 
 void set_sample_fn(int16_t (*fn)(int)) {
@@ -29,24 +28,26 @@ void set_sample_fn(int16_t (*fn)(int)) {
 }
 
 static int16_t get_track_pitch(int i) {
+	static int samples[TRACKS] = {0, 0, 0, 0};
+
 	int16_t sound = 0;
 
 
 	// If note is done
-	if (notes[i] && notes[i]->progress >= notes[i]->duration) {
-		notes[i] = notes[i]->next; // Is NULL when tune is done
+	if (tracks[i] && tracks[i]->progress >= tracks[i]->duration) {
+		tracks[i] = tracks[i]->next; // Is NULL when tune is done
 	}
 
 	// Check if tune is done
-	if (notes[i] == NULL) {
+	if (tracks[i] == NULL) {
 
 	} else {
 		// If within 7/8 or the tone play it
-		if (notes[i]->progress <= (int16_t)(notes[i]->duration * notes[i]->cutoff)) {
-                  sound = (*sample_fn)(samples[i]);
+		if (tracks[i]->progress <= (int16_t)(tracks[i]->duration * tracks[i]->cutoff)) {
+		  sound = (*sample_fn)(samples[i]);
 		}
-		notes[i]->progress++;
-		samples[i] += notes[i]->pitch;
+		tracks[i]->progress++;
+		samples[i] += tracks[i]->pitch;
 
 		if (samples[i] >= SAMPLES) {
 			samples[i] = 0;
@@ -60,18 +61,18 @@ int16_t get_playback_pitch() {
 
 	int16_t sound = 0;
 	int i;
-        int notNULL = 0;
+	int notNULL = 0;
 
 	for (i=0; i<TRACKS; i++) {
 		sound += get_track_pitch(i);
-                if (notes[i] != NULL) {
-                  notNULL = 1;
-                }
+		if (tracks[i] != NULL) {
+		  notNULL = 1;
+		}
 	}
 
-        if (!notNULL) {
-          turn_off_abdac();
-        }
+	if (!notNULL) {
+	  turn_off_abdac();
+	}
 
 	return sound;
 }
