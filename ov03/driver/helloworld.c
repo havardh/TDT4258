fe51ba23cc8252e2f18be382f7e6cd9b811bbf58
Håvard Wormdal Høiby <havardwhoiby@gmail.com>
2013-04-09 18:33:13 +0200

    Ble ikke ferdig med å compilere kjernen...

--- ov03/driver/helloworld.c
+++ ov03/driver/helloworld.c
@@ -1,18 +1,21 @@
-#include <linux/init.h>
 #include <linux/module.h>
+//#include <linux/version.h>
+//#include <linux/kernel.h>
 
-MODULE_LICENSE("Dual BSD/GPL");
 
-static int hello_init(void)
+static int __init hello_init(void)
 {
-	printk(KERN_ALERT "Hello, world\n");
+	printk(KERN_INFO "Hello, world\n");
 	return 0;
 }
 
-static void hello_exit(void)
+static void __exit hello_exit(void)
 {
-	printk(KERN_ALERT "Goodbye, cruel world\n");
+	printk(KERN_INFO "Goodbye, cruel world\n");
 }
 
 module_init(hello_init);
 module_exit(hello_exit);
+
+
+MODULE_LICENSE("GPL");
