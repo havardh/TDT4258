d0a30a179258cd28d3b1eaee1dbef971d6322e20
Håvard Wormdal Høiby <havardwhoiby@gmail.com>
2013-04-23 10:41:31 +0200

    Added tank and cannon bmp

--- ov03/game/src/main.c
+++ ov03/game/src/main.c
@@ -12,11 +12,7 @@ int main ( void ) {
 	Screen screen = ScreenNew(320, 240);
 	Canvas canvas = CanvasNew( &screen );
 	Controller ctrl = ControllerNew( &canvas );
-        //Image *image = ImageNew("../graphics/data/box.bmp", 10, 10); // "./data/field.bmp"
-        
-        //CanvasAdd( &canvas, image );
-        //CanvasPaint( &canvas );
- 
+
 	onGameInit( &ctrl );
 	onGameExit( &ctrl );
 
