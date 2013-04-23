2c2578900268fdfaa33e7ab30bdf32725db0b1ed
Andy Philipp <philipp.andy@gmail.com>
2013-04-23 14:44:17 +0200

    read

--- ov03/driver/buttons/buttons_driver.c
+++ ov03/driver/buttons/buttons_driver.c
@@ -12,6 +12,8 @@
 #include "buttons_driver.h"
 #include "../ap7000.h"
 
+volatile avr32_pio_t *btn_pio = &AVR32_PIOB;
+
 static irqreturn_t button_interrupt(int irq, void *dev_id)
 {
 	printk(KERN_ALERT "int %d: interupt received. Irq number: %d\n", -EBUSY,AVR32_PIOB_IRQ);
@@ -20,39 +22,55 @@ static irqreturn_t button_interrupt(int irq, void *dev_id)
 
 static int __init button_init(void)
 {	
+	
+/*	INIT
+	volatile int isr;
+    btn_pio->per |= 0xff;
+    btn_pio->puer |= 0xff;
+    btn_pio->ier |= 0xff;
+    isr = btn_pio->isr;
+*/	
 	printk(KERN_ALERT "Module started\n");
+
 	printk(KERN_ALERT "Requesting GPIO %d\n",AVR32_PIOB_ADDRESS);
-	printk(KERN_ALERT "Requesting Irq %d\n",AVR32_PIOB_IRQ);
-	
-	unsigned int GPIO_REQUEST;
-	
-	GPIO_REQUEST = gpio_request(AVR32_PIOB_ADDRESS, "buttons");
+	unsigned int GPIO_REQUEST = gpio_request(AVR32_PIOB_ADDRESS, "buttons");
 	if (GPIO_REQUEST < 0) {
 		printk(KERN_ALERT "error %d: could not request gpio: %d\n", GPIO_REQUEST,AVR32_PIOB_ADDRESS);
 		return GPIO_REQUEST;
 	}
 	
+	/*
+	printk(KERN_ALERT "Requesting Irq %d\n",AVR32_PIOB_IRQ);
 	//flags: 0, SA_INTERRUPT, SA_ONESHOT or SA_PROBE.
 	int IRQ_REQUEST = request_irq(AVR32_PIOB_IRQ, button_interrupt, 0, "buttons", NULL);
-<<<<<<< HEAD
-	
-=======
->>>>>>> IRQ448
 	if (IRQ_REQUEST) {
 		printk(KERN_ALERT "error %d: could not request irq: %d\n", IRQ_REQUEST, AVR32_PIOB_IRQ);
 		return IRQ_REQUEST;
 	}
-	
+	*/
 	return 0;
 } 
 
 static void __exit button_exit(void)
 {
-	printk(KERN_ALERT "exit : removing irq: %d\n",AVR32_PIOB_IRQ);
+	//printk(KERN_ALERT "exit : removing irq: %d\n",AVR32_PIOB_IRQ);
 	printk(KERN_ALERT "exit : removing button: %d\n",AVR32_PIOB_ADDRESS);
-	free_irq(AVR32_PIOB_IRQ,  NULL);
+	//free_irq(AVR32_PIOB_IRQ,  NULL);
 	gpio_free(AVR32_PIOB_ADDRESS);
 } 
+
+
+static ssize_t driver_read (struct file *filp, char __user *buff, size_t count, loff_t *offp)
+{
+    char output;
+    if (count == 0)
+        return 0;
+    output = ~btn_pio->pdsr;
+    copy_to_user(buff, &output, 1);
+    *offp += 1;
+    return 1;
+}
+
 
 module_init(button_init);
 module_exit(button_exit);
