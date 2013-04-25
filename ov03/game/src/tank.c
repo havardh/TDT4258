324d1c0848511535a0815cf353031d524beedad5
Håvard Wormdal Høiby <havardwhoiby@gmail.com>
2013-04-25 10:56:20 +0200

    Added tank directional images

--- ov03/game/src/tank.c
+++ ov03/game/src/tank.c
@@ -3,9 +3,28 @@
 static void paint ( Shape *shape, Screen *screen ) {
 
 	Tank *tank = (Tank*)shape;
-	tank->image->x = tank->x * 20;
-	tank->image->y = tank->y * 20;
-	tank->image->paint( tank->image, screen );
+	switch (tank->direction) {
+	case E:
+		tank->image_e->x = tank->x * 20;
+		tank->image_e->y = tank->y * 20;
+		tank->image_e->paint( tank->image, screen );
+		break;
+	case N:
+		tank->image_n->x = tank->x * 20;
+		tank->image_n->y = tank->y * 20;
+		tank->image_n->paint( tank->image, screen );
+		break;
+	case W:
+		tank->image_w->x = tank->x * 20;
+		tank->image_w->y = tank->y * 20;
+		tank->image_w->paint( tank->image, screen );
+		break;
+	case S:
+		tank->image_s->x = tank->x * 20;
+		tank->image_s->y = tank->y * 20;
+		tank->image_s->paint( tank->image, screen );
+		break;
+	}
 
 }
 
@@ -14,7 +33,13 @@ Tank TankNew( int x, int y ) {
 	Tank tank = {
 		.parent = NULL,
 		.paint = &paint,
-		.image = ImageNew(tank_filename, x * 20, y * 20),
+		.image_e = ImageNew(tank_e_filename, x * 20, y * 20),
+		.image_n = ImageNew(tank_n_filename, x * 20, y * 20),
+		.image_w = ImageNew(tank_w_filename, x * 20, y * 20),
+		.image_s = ImageNew(tank_s_filename, x * 20, y * 20),
+
+		.direction = N,
+
 		.startx = x,
 		.starty = y,
 
