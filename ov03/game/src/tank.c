768c57e9e26ade9a52232688de48dad8903a2b4c
Håvard Wormdal Høiby <havardwhoiby@gmail.com>
2013-04-25 10:57:13 +0200

    Corrected tank direcetion

--- ov03/game/src/tank.c
+++ ov03/game/src/tank.c
@@ -7,22 +7,22 @@ static void paint ( Shape *shape, Screen *screen ) {
 	case E:
 		tank->image_e->x = tank->x * 20;
 		tank->image_e->y = tank->y * 20;
-		tank->image_e->paint( tank->image, screen );
+		tank->image_e->paint( tank->image_e, screen );
 		break;
 	case N:
 		tank->image_n->x = tank->x * 20;
 		tank->image_n->y = tank->y * 20;
-		tank->image_n->paint( tank->image, screen );
+		tank->image_n->paint( tank->image_n, screen );
 		break;
 	case W:
 		tank->image_w->x = tank->x * 20;
 		tank->image_w->y = tank->y * 20;
-		tank->image_w->paint( tank->image, screen );
+		tank->image_w->paint( tank->image_w, screen );
 		break;
 	case S:
 		tank->image_s->x = tank->x * 20;
 		tank->image_s->y = tank->y * 20;
-		tank->image_s->paint( tank->image, screen );
+		tank->image_s->paint( tank->image_s, screen );
 		break;
 	}
 
