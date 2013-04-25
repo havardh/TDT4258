f893753b547621584398004cb7a048214d9a88d3
Håvard Wormdal Høiby <havardwhoiby@gmail.com>
2013-04-25 13:16:56 +0200

    Refactored main.c

---
+++ ov03/game/include/splash.h
@@ -0,0 +1,9 @@
+#ifndef _SPLASH_H_
+#define _SPLASH_H_
+
+#include "image.h"
+#include "canvas.h"
+
+void showSplashScreen( Canvas* );
+
+#endif // _SPLASH_H_
--- ov03/game/src/main.c
+++ ov03/game/src/main.c
@@ -6,76 +6,7 @@
 //#include "led.h"
 #include "button.h"
 
-static Controller ctrl;
 static bool running = 1;
-
-static int tankmode = 1;
-
-/* Tank player button actions */
-static void tr( void ) { if (tankmode) { onTankMove( &ctrl, 1, 0 ); } else { onTankMove( &ctrl, -1, 0 ); } }
-static void td( void ) { if (tankmode) { onTankMove( &ctrl,  0,-1 ); } else { onTankMove( &ctrl,  0, 1 ); } }
-static void tu( void ) { tankmode = (tankmode) ? 0 : 1; }
-
-/* Cannon player button actions */
-static void cr( void ) { onCannonAim( &ctrl,  1, 0 ); }
-static void cl( void ) { onCannonAim( &ctrl, -1, 0 ); }
-static void cu( void ) { onCannonAim( &ctrl,  0,-1 ); }
-static void cd( void ) { onCannonAim( &ctrl,  0, 1 ); }
-static void cannon_fire( void ) { onCannonFire( &ctrl); }
-
-/* Register callback on button module */
-static void RegisterCallbacks( void ) {
-
-	ButtonAddCallback( 0, &tr );
-	ButtonAddCallback( 1, &td );
-	ButtonAddCallback( 2, &tu );
-
-	ButtonAddCallback( 3, &cannon_fire );
-	ButtonAddCallback( 4, &cr );
-	ButtonAddCallback( 5, &cu );
-	ButtonAddCallback( 6, &cd );
-	ButtonAddCallback( 7, &cl );
-}
-
-void showSplashScreen( Canvas *canvas ) {
-
-	Image *splash0 = ImageNew( "./data/splash_0.bmp", 0, 0 );
-	CanvasAdd( canvas, splash0 );
-	CanvasPaint( canvas );
-	sleep( 1 );
-	CanvasRemove( canvas, splash0 );
-	Image *splash1 = ImageNew( "./data/splash_1.bmp", 0, 0 );
-	CanvasAdd( canvas, splash1 );
-	CanvasPaint( canvas );
-	sleep( 1 );
-	CanvasRemove( canvas, splash1 );
-	Image *splash2 = ImageNew( "./data/splash_2.bmp", 0, 0 );
-	CanvasAdd( canvas, splash2 );
-	CanvasPaint( canvas );
-	sleep( 1 );
-	CanvasRemove( canvas, splash2 );
-	Image *splash3 = ImageNew( "./data/splash_3.bmp", 0, 0 );
-	CanvasAdd( canvas, splash3 );
-	CanvasPaint( canvas );
-	sleep( 1 );
-	CanvasRemove( canvas, splash3 );
-	Image *splash4 = ImageNew( "./data/splash_4.bmp", 0, 0 );
-	CanvasAdd( canvas, splash4 );
-	CanvasPaint( canvas );
-	sleep( 1 );
-	CanvasRemove( canvas, splash4 );
-	Image *splash5 = ImageNew( "./data/splash_5.bmp", 0, 0 );
-	CanvasAdd( canvas, splash5 );
-	CanvasPaint( canvas );
-	sleep( 1 );
-	CanvasRemove( canvas, splash5 );
-	Image *splash6 = ImageNew( "./data/splash_6.bmp", 0, 0 );
-	CanvasAdd( canvas, splash6 );
-	CanvasPaint( canvas );
-	sleep( 1 );
-	CanvasRemove( canvas, splash6 );
-
-}
 
 int main ( void ) {
 
@@ -90,8 +21,8 @@ int main ( void ) {
 	//Audio audio = AudioNew();
 
 	// Init game
-	ctrl = ControllerNew( &canvas, NULL );
-	RegisterCallbacks();
+	Controller ctrl = ControllerNew( &canvas, NULL );
+	RegisterCallbacks( &ctrl );
 
 	showSplashScreen( &canvas );
 
