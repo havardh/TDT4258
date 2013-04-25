2fd3647ae09aaca06787fa012911852f13c20e73
Håvard Wormdal Høiby <havardwhoiby@gmail.com>
2013-04-25 11:29:40 +0200

    Added angle to cannon

--- ov03/game/src/cannon.c
+++ ov03/game/src/cannon.c
@@ -4,7 +4,17 @@ static void paint ( Shape *shape, Screen *screen ) {
 
 	Cannon *cannon = (Cannon*)shape;
 
-	cannon->image->paint( cannon->image, screen );
+	if (cannon->angle == 0) {
+		cannon->image->paint( cannon->image, screen );
+	} else if (cannon->angle == 30) {
+		cannon->image->paint( cannon->image, screen );
+	} else if (cannon->angle == 45) {
+		cannon->image->paint( cannon->image, screen );
+	} else if (cannon->angle == 60) {
+		cannon->image->paint( cannon->image, screen );
+	} else if (cannon->angle == 90) {
+		cannon->image->paint( cannon->image, screen );
+	}
 
 	cannon->aim_image->x = cannon->aimx * 20;
 	cannon->aim_image->y = cannon->aimy * 20;
@@ -25,7 +35,7 @@ Cannon CannonNew( int x, int y ) {
 		.aimx = x-1,
 		.aimy = y+1,
 		.health = 4,
-		.angle = 0
+		.angle = 45
 	};
 
 	return cannon;
@@ -41,6 +51,14 @@ void CannonAim( Cannon *cannon, int dx, int dy ) {
 
 	cannon->aimx += dx;
 	cannon->aimy += dy;
+
+	if ( cannon->aimx == 0 ) {
+		cannon->angle = 0;
+	} else if ( cannon->y == 15 ) {
+		cannon->angle = 90;
+	} else {
+		cannon->angle = 45;
+	}
 
 }
 
