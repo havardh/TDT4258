ad32e4b79ff0b887f26b2fc4804094ce15574995
Håvard Wormdal Høiby <havardwhoiby@gmail.com>
2013-04-25 23:47:37 +0200

    Fixed sound test program

--- ov03/sounds/test/main.c
+++ ov03/sounds/test/main.c
@@ -3,16 +3,13 @@
 
 int main( void ) {
 
-	Audio audio = AudioNew();
-
 	//Sample sample = SampleNew("./data/8k8bitpcm.wav");
 	char *sample1 = "./data/sample1.wav";
 	char *sample2 = "./data/sample2.wav";
 
-	Play( &audio, sample1 );
+	AudioPlay( sample1 );
 	sleep( 2 );
-	Play( &audio, sample2 );
+	AudioPlay( sample2 );
 
-	AudioDestroy( &audio );
 	return 0;
 }
